24b46dbd1d2eec3499a453bf0b608ecd73c064b6
mmantle-hud <m.e.mantle@hud.ac.uk>
2021-12-02 15:34:55 +0000

    Update index.php

--- ActiveRecord/index.php
+++ ActiveRecord/index.php
@@ -5,4 +5,4 @@ require_once("Film.php");
 
 //get one film
 $film=Film::find(10);
-echo "<p>".$film->title." is ".$film->getAge()." years old</p>";
+echo "<p>{$film->title} is {$film->getAge()} years old</p>";
